b67afe46b8f8f23a8cb06984519b5ed1c9390e91
mst <nzero8198@gmail.com>
2025-01-27 10:17:51 +0100

    updating packages

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import MainPage from './mainPage/mainPage'
-export default function App(){
-  return (
-   <MainPage />
-  )
-}
-
--- src/countries page/details/countries.jsx
+++
@@ -1,8 +0,0 @@
-export default function Countries(){
-        return(
-          <div className="countries">
-            <input type="text" className='p-2 px-4 rounded-pill  border border-none' />
-            <button className='btn btn bg-primary w-50 text-white'>print</button>
-          </div>
-    )
-}
